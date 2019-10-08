83dde04f36e637803d8121ede6ba6779108c9031
CrabBucket <tom@mcratracing.com>
2019-10-08 12:12:45 -0700

    Done

--- BigSumm.py
+++ BigSumm.py
@@ -1,12 +1,31 @@
-git alist = []
-for string in sys.stdin:
-    list.append(string)
-
-
+list = []
+import sys
 def stringAdd(string,result):
-    index = len(string) - 1
-    for char in string:
-        temp = ord(char) - 48
-        result[index] = result[index] + temp
-        if(result[index]>9):
-            result[index]
+
+
+    for i in range(len(string))[::-1]:
+
+        temp = ord(string[i]) - 48
+
+        result[i] = result[i] + temp
+        if(result[i]>9 and i>0):
+            result[i] = result[i] - 10
+            result[i-1] += 1
+
+
+
+
+    return result
+for i in range(57):
+    list.append(0)
+
+for string in sys.stdin:
+    if(string[0]=='#'):
+        break
+    string = "000000" + string
+    print(string)
+    list = stringAdd(string,list)
+
+
+for x in list[4:14]:
+    print(x,end='')
